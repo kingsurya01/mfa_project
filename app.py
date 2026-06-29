89e344356815d5fdaf1ae2254494b50cd1e3e63e
Surya <janakisurya1106@gmail.com>
2026-06-29 13:32:04 +0530

    testing

--- app.py
+++ app.py
@@ -76,7 +76,16 @@ CAPTCHAS = [
     } 
 ]
   
+import socket
 
+@app.route("/test")
+def test():
+    try:
+        socket.create_connection(("smtp.gmail.com", 587), timeout=10)
+        return "SMTP reachable"
+    except Exception as e:
+        return str(e)
+    
 def get_new_captcha():
 
     available = session.get(
--- test.py
+++
@@ -1,9 +0,0 @@
-import socket
-
-@app.route("/test")
-def test():
-    try:
-        socket.create_connection(("smtp.gmail.com", 587), timeout=10)
-        return "SMTP reachable"
-    except Exception as e:
-        return str(e)
